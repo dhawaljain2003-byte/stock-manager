b3c788efadca0c2f2327aa063e0294f1f5628996
Dhawal Jain <dhawaljain2003@gmail.com.com>
2025-09-03 01:36:12 +0530

    Updated stock manager app with new changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPjFmyR8u-MFlkz9_mG75CninvcXwb9K8",
-  authDomain: "stock-manager-ef5bf.firebaseapp.com",
-  projectId: "stock-manager-ef5bf",
-  storageBucket: "stock-manager-ef5bf.firebasestorage.app",
-  messagingSenderId: "591023777922",
-  appId: "1:591023777922:web:c6c08a6e497b01a3dd6c9b",
-  measurementId: "G-ZCN6W2TFDZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
